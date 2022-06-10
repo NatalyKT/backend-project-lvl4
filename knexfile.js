310a786a33be4564e3f8be06f508885fd911654d
nataly_kt <nataly_kt@hotmail.com>
2022-06-10 23:54:51 +0300

    add Eslint, upd package.json

--- knexfile.js
+++ knexfile.js
@@ -1,35 +1,35 @@
 // @ts-check
 
-import path from 'path';
-import { fileURLToPath } from 'url';
+import path from "path";
+import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const migrations = {
-  directory: path.join(__dirname, 'server', 'migrations'),
+  directory: path.join(__dirname, "server", "migrations"),
 };
 
 export const development = {
-  client: 'sqlite3',
+  client: "sqlite3",
   connection: {
-    filename: './database.sqlite',
+    filename: "./database.sqlite",
   },
   useNullAsDefault: true,
   migrations,
 };
 
 export const test = {
-  client: 'sqlite3',
-  connection: ':memory:',
+  client: "sqlite3",
+  connection: ":memory:",
   useNullAsDefault: true,
   // debug: true,
   migrations,
 };
 
 export const production = {
-  client: 'sqlite3',
+  client: "sqlite3",
   connection: {
-    filename: './database.sqlite',
+    filename: "./database.sqlite",
   },
   useNullAsDefault: true,
   migrations,
--- server/models/User.cjs
+++ server/models/User.cjs
@@ -1,24 +1,24 @@
 // @ts-check
 
-const BaseModel = require('./BaseModel.cjs');
-const objectionUnique = require('objection-unique');
-const encrypt = require('../lib/secure.cjs');
+const BaseModel = require("./BaseModel.cjs");
+const objectionUnique = require("objection-unique");
+const encrypt = require("../lib/secure.cjs");
 
-const unique = objectionUnique({ fields: ['email'] });
+const unique = objectionUnique({ fields: ["email"] });
 
 module.exports = class User extends unique(BaseModel) {
   static get tableName() {
-    return 'users';
+    return "users";
   }
 
   static get jsonSchema() {
     return {
-      type: 'object',
-      required: ['email', 'password'],
+      type: "object",
+      required: ["email", "password"],
       properties: {
-        id: { type: 'integer' },
-        email: { type: 'string', minLength: 1 },
-        password: { type: 'string', minLength: 3 },
+        id: { type: "integer" },
+        email: { type: "string", minLength: 1 },
+        password: { type: "string", minLength: 3 },
       },
     };
   }
@@ -30,4 +30,4 @@ module.exports = class User extends unique(BaseModel) {
   verifyPassword(password) {
     return encrypt(password) === this.passwordDigest;
   }
-}
+};
