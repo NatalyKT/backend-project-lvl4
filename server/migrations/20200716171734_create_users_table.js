8a4556d232a5756c359fc5df5414725e78c02726
nataly_kt <nataly_kt@hotmail.com>
2022-06-13 01:43:57 +0300

    fix some bugs

--- server/migrations/20200716171734_create_users_table.js
+++ server/migrations/20200716171734_create_users_table.js
@@ -3,8 +3,8 @@
 export const up = (knex) =>
   knex.schema.createTable("users", (table) => {
     table.increments("id").primary();
-    table.string('first_name')
-    table.sting('last_name')
+    table.string('first_name');
+    table.sting('last_name');
     table.string("email");
     table.string("password_digest");
     table.timestamp("created_at").defaultTo(knex.fn.now());
