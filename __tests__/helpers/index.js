9c8b7d4277b3a97a8bf5562e56dd95cb05a134a4
nataly_kt <nataly_kt@hotmail.com>
2022-06-15 00:53:41 +0300

    fix tests

--- __tests__/helpers/index.js
+++ __tests__/helpers/index.js
@@ -5,14 +5,12 @@ import fs from 'fs';
 import path from 'path';
 
 const getFixturePath = (filename) => path.join('..', '..', '__fixtures__', filename);
-const readFixture = (filename) => fs
-  .readFileSync(new URL(getFixturePath(filename), import.meta.url), 'utf-8')
-  .trim();
+const readFixture = (filename) => fs.readFileSync(new URL(getFixturePath(filename), import.meta.url), 'utf-8').trim();
 const getFixtureData = (filename) => JSON.parse(readFixture(filename));
 
-const getTestData = () => getFixtureData('testData.json');
+export const getTestData = () => getFixtureData('testData.json');
 
-const prepareData = async (app) => {
+export const prepareData = async (app) => {
   const { knex } = app.objection;
 
   await knex('users').insert(getFixtureData('users.json'));
@@ -22,18 +20,16 @@ const prepareData = async (app) => {
   await knex('tasks_labels').insert(getFixtureData('tasksLabels.json'));
 };
 
-const signIn = async (app, testData) => {
-  const responseSignIn = await app.inject({
+export const signIn = async (app, data) => {
+  const response = await app.inject({
     method: 'POST',
     url: app.reverse('session#create'),
     payload: {
-      data: testData,
+      data,
     },
   });
 
-  const [sessionCookie] = responseSignIn.cookies;
+  const [sessionCookie] = response.cookies;
   const { name, value } = sessionCookie;
   return { [name]: value };
 };
-
-export { getTestData, prepareData, signIn };
